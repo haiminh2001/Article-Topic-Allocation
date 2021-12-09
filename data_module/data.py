db27fb26fe14b27a7385ae7f5bdb92b05da0362f
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-12-09 10:24:04 +0700

    upgrade data holder

--- data_module/data.py
+++ data_module/data.py
@@ -9,27 +9,53 @@ class DataHolder(ABC):
         self.read_csv()
     
     def read_csv(self):
-        self.data = {'texts': [], 'labels': []}
-        self.data = pd.DataFrame(self.data)
+        self.data = {}
+        self.data['train'] = {'texts': [], 'labels': []}
+        self.data['train'] = pd.DataFrame(self.data)
+        self.data['test'] = {'texts': [], 'labels': []}
         print('Loading data...')
         for file in tqdm(os.listdir(self.data_folder)):
             if file.endswith('.csv'):
                 df = pd.read_csv(self.data_folder + '/' +  file)
                 texts = df.iloc[: , 1 ]
                 labels = df.iloc[: , 0 ]
-                self.data = self.data.append(pd.DataFrame({'texts': texts, 'labels': labels}))
+                if file.startswtih('test'):
+                    self.data['test'] = self.data['test'].append(pd.DataFrame({'texts': texts, 'labels': labels}))
+                else:
+                    self.data['train'] = self.data['train'].append(pd.DataFrame({'texts': texts, 'labels': labels}))
                 
     @property
-    def texts(self):
-        return self.data['texts']
+    def train_texts(self):
+        return self.data['train']['texts']
+    
+    @property
+    def train_labels(self):
+        return self.data['train']['labels']
+    
+    @property
+    def test_texts(self):
+        return self.data['test']['texts']
+    
+    @property
+    def test_labels(self):
+        return self.data['test']['texts']
     
     def __str__(self) -> str:
-        info = 'columns: '
+        info = 'Data summary:\ntrain data:\ncolumns: '
         
         for column in self.data.columns:
             info += str(column) + ', '
         
-        info += 'rows: ' + str(self.data.shape[0])
+        info += 'rows: ' + str(self.data['train'].shape[0])
         info += '\nnum labels: \n'
-        info += str(self.data['labels'].value_counts())
+        info += str(self.data['train']['labels'].value_counts())
+        
+        info = '\ntest data:\ncolumns: '
+        
+        for column in self.data.columns:
+            info += str(column) + ', '
+        
+        info += 'rows: ' + str(self.data['test'].shape[0])
+        info += '\nnum labels: \n'
+        info += str(self.data['test']['labels'].value_counts())
         return info
