d4a243990000da8b4306408394f71a5f8bd73c90
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-12-15 09:59:06 +0700

    add save classifier

--- model_module/Classifier.py
+++ model_module/Classifier.py
@@ -1,7 +1,9 @@
+from urllib.parse import uses_relative
 from pytorch_lightning import trainer
 import torch
 import pytorch_lightning as pl
 from torch.nn.modules import dropout
+from torch.serialization import load
 from data_module import ClassifierInputDataset, DataHolder, data
 from os.path import dirname, abspath
 import pickle
@@ -17,6 +19,8 @@ tensors_folder = '/data_module/saved_data/temp_tensors'
 info_train_file = '/data_module/saved_data/embed_train_info.pickle'
 info_test_file = '/data_module/saved_data/embed_test_info.pickle'
 num_labels = 13
+hprams_file= '/data_module/saved_data/classifer_hprams.pickle'
+model_file= '/data_module/saved_data/classifier.ckpt'
 
 class Classifier():
     def __init__(self,
@@ -25,32 +29,46 @@ class Classifier():
                  eval_batch_size:int = 1024,
                  dropout:float = 0.1,
                  valid_split:float = 0.2,
-                 sequence_length: int = 20,
-                 sequence_overlapping:int = 3,
                  lr:float = 1e-3,
                  eps:float = 1e-5,
                  gpus:int =1, 
-                 epochs:int = 5,
                  use_lr_finder:bool = True,
+                 load_classifier:bool= False,
+                 cfg_optimizer:bool= False,
                  ):
         super(Classifier, self).__init__()
         print('Collecting data information...')
-        self.use_lr_finder= use_lr_finder
-        self.num_train_datasets = 0
-        self.num_test_datasets = 0
-        self.count_dataset()
-        self.num_workers = num_workers
-        self.train_batch_size = train_batch_size
-        self.eval_batch_size = eval_batch_size
-        self.dropout = dropout
-        self.valid_split = valid_split
-        self.sequence_length = sequence_length
-        self.sequnce_overlapping = sequence_overlapping
-        self.model_set_upped = False
-        self.lr = lr
-        self.eps = eps
-        self.gpus = gpus
-        self.epochs = epochs
+        if load_classifier:
+            if cfg_optimizer:
+                self.load(lr= lr,
+                          eps= eps,
+                          dropout= dropout,
+                          use_lr_finder= use_lr_finder,
+                          valid_split= valid_split,
+                          train_batch_size= train_batch_size,
+                          eval_batch_size= eval_batch_size,
+                          )
+            else:
+                self.load()
+                self.gpus = gpus
+                self.hprams = None
+        else:
+            self.hprams = locals()
+            del self.hprams['load_embedder']
+            del self.hprams['cfg_optimizer']
+            del self.hprams['self']
+            self.num_train_datasets = 0
+            self.num_test_datasets = 0
+            self.count_dataset()
+            self.num_workers = num_workers
+            self.train_batch_size = train_batch_size
+            self.eval_batch_size = eval_batch_size
+            self.dropout = dropout
+            self.valid_split = valid_split
+            self.model_set_upped = False
+            self.lr = lr
+            self.eps = eps
+            self.gpus = gpus
     
     def setup_train_data(self, valid_split:float= 0.2, index:int= 0):
         
@@ -131,6 +149,43 @@ class Classifier():
     
     def forward(self, x):
         return self.classifier(x)
+    
+    def save(self):
+        #save network
+        self.trainer.save_checkpoint(dir_path + model_file, weights_only= True)
+        if self.hprams:
+            #save model hprams
+            with open(dir_path + hprams_file, 'wb+') as f:
+                pickle.dump(self.hprams, f)
+        print('Saved word embedder')
+        
+    def load(self, **optim_params):
+        print('Loading classifier...')
+        kwargs = None
+        with open (dir_path + hprams_file, 'rb') as f:
+            kwargs = pickle.load(f)
+        if kwargs:
+            if optim_params:
+                for attribute in optim_params.keys():
+                    kwargs[attribute] = optim_params[attribute]
+            lr = kwargs['lr']
+            eps = kwargs['eps']
+            dropout = kwargs['dropout']
+            use_lr_finder= kwargs['use_lr_finder']
+            valid_split = kwargs['valid_split']
+            train_batch_size= kwargs['train_batch_size']
+            eval_batch_size = kwargs['eval_batch_size']
+            self.model =  Classifier.load_from_checkpoint(dir_path + model_file, 
+                                                          lr= lr, 
+                                                          eps= eps, 
+                                                          dropout= dropout,
+                                                          use_lr_finder= use_lr_finder,
+                                                          valid_split= valid_split,
+                                                          train_batch_size= train_batch_size,
+                                                          eval_batch_size= eval_batch_size,
+                                                          )
+        else:
+            print('No classifier found')
             
         
 class SimpleClassifier(pl.LightningModule):
