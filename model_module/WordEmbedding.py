dcc09c29116d063a498329495a6cd95e3e07ec2b
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-12-15 07:48:52 +0700

    adjust

--- model_module/WordEmbedding.py
+++ model_module/WordEmbedding.py
@@ -255,6 +255,7 @@ class WordEmbedder():
             self.vocab_builder = vocab_builder
             self.gpus = gpus
             self.hprams = None
+            self.use_lr_finder= use_lr_finder
             
         else:
             self.model = WordEmbeddingModel(max_vocab_length= max_vocab_length, embedding_dim= embedding_dim, num_heads= num_heads, window_size= window_size, dropout= dropout, lr = lr, eps = eps, hide_target_rate= hide_target_rate)
