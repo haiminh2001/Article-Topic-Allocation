3dbb3cb51b95b74b5b8d770ae3966dc00422a821
haiminh2001 <haiminhnguyen2001@gmail.com>
2021-12-31 15:37:38 +0700

    fix bugs

--- model_module/Classifier.py
+++ model_module/Classifier.py
@@ -206,7 +206,7 @@ class Classifier():
                 if i!= 0:
                     update_data = True
             if update_data:
-                self.setup_model()
+                self.setup_train_data(valid_split= self.valid_split, index= i + 1)
                 self.classifier.current_epoch = 0
         
             self.trainer.fit(
